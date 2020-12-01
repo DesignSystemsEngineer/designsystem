b7910b56877aaebf997406d8b81165ba177d2168
Garth Braithwaite <garthdb@gmail.com>
2020-11-30 23:33:40 -0700

    feat!: fixed tokens with scales

--- packages/tokens/build.js
+++ packages/tokens/build.js
@@ -1,60 +1,110 @@
-const StyleDictionaryPackage = require("style-dictionary");
+const StyleDictionary = require("style-dictionary");
+const CSSVarsFormatter = require("./css-vars-formatter");
 
-// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+StyleDictionary.registerFormat({
+  name: "css/deep-variables",
+  formatter: CSSVarsFormatter,
+});
 
-function getStyleDictionaryConfig(source, destination) {
-  return {
-    source: [source],
-    platforms: {
-      css: {
-        transforms: [
-          "attribute/cti",
-          "name/cti/kebab",
-          "time/seconds",
-          "content/icon",
-          "color/css",
-        ],
-        buildPath: `lib/`,
-        prefix: "dse",
+StyleDictionary.registerTransform({
+  name: "attribute/sets",
+  type: "attribute",
+  matcher: (prop) => {
+    return prop.hasOwnProperty("sets");
+  },
+  transformer: (prop, options) => {
+    const attributes = prop.attributes || {};
+    attributes.sets = prop.sets;
+    return attributes;
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: "value/sets",
+  type: "value",
+  matcher: (prop) => {
+    return prop.attributes.hasOwnProperty("sets");
+  },
+  transformer: (prop, options) => {
+    let value = prop.value;
+    for (const setKey in prop.sets) {
+      if (options.sets.includes(setKey)) {
+        value = prop.attributes.sets[setKey];
+      }
+    }
+    return value;
+  },
+});
+
+const setRegex = /@(\w+):(\w+)/;
+
+const defaultFile = {
+  destination: "default.css",
+  options: { showFileHeader: false },
+  format: "css/deep-variables",
+  filter: (prop) => {
+    return prop.attributes.type !== "icon";
+  },
+};
+const defaultPlatform = {
+  transforms: [
+    "attribute/sets",
+    "value/sets",
+    "attribute/cti",
+    "name/cti/kebab",
+    "time/seconds",
+    "content/icon",
+  ],
+  buildPath: "lib/",
+  deep: true,
+  sets: ["large"],
+  prefix: "dse",
+  files: [defaultFile],
+};
+
+const config = {
+  source: ["properties/**/*.json"],
+  platforms: {
+    "CSS: Scale Medium": {
+      ...defaultPlatform,
+      ...{
+        sets: ["medium"],
         files: [
           {
-            destination: destination,
-            format: "css/variables",
-            options: { showFileHeader: false },
+            ...defaultFile,
+            ...{
+              destination: "medium.css",
+              filter: (prop) => {
+                return (
+                  prop.attributes.hasOwnProperty("sets") &&
+                  prop.attributes.sets.hasOwnProperty("medium")
+                );
+              },
+            },
           },
         ],
       },
     },
-  };
-}
-
-console.log("Tokens build started...");
-
-[
-  {
-    name: "medium",
-    source: "properties/scales/medium/*.json",
-    destination: "medium.css",
+    "CSS: Default with Large Scale": defaultPlatform,
+    "Assets: Icons": {
+      transforms: ["attribute/cti", "name/cti/kebab", "asset/base64"],
+      buildPath: "lib/",
+      prefix: "dse",
+      files: [
+        {
+          destination: "assets_icons.css",
+          format: "css/deep-variables",
+          filter: {
+            attributes: {
+              category: "asset",
+              type: "icon",
+            },
+          },
+        },
+      ],
+    },
   },
-  {
-    name: "large",
-    source: "properties/scales/large/*.json",
-    destination: "large.css",
-  },
-  {
-    name: "globals",
-    source: "properties/globals/**/*.json",
-    destination: "globals.css",
-  },
-].map(function (tokenGroup) {
-  console.log("\n==============================================");
-  console.log(`\nProcessing: [${tokenGroup.name}]`);
-  const StyleDictionary = StyleDictionaryPackage.extend(
-    getStyleDictionaryConfig(tokenGroup.source, tokenGroup.destination)
-  );
-  StyleDictionary.buildAllPlatforms();
-  console.log("\nEnd processing");
-});
+};
 
-console.log("\n==============================================");
-console.log("\nBuild completed!");
+StyleDictionaryExtended = StyleDictionary.extend(config);
+StyleDictionaryExtended.buildAllPlatforms();
